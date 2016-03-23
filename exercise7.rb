51fb6f9b4e493f584000d49e397d26cd598eca8b
CALLUM BURNS <CBU787@GMAIL.COM>
2016-03-23 17:43:37 -0400

    Calculated and output total number of students.

--- exercise7.rb
+++ exercise7.rb
@@ -6,15 +6,22 @@ students = {
 
 students[:cohort4] = 43
 
-students.each do |k, v|
-  puts "#{k}: #{v} students"
+# students.each do |k, v|
+#   puts "#{k}: #{v} students"
+# end
+#
+# puts students.keys
+#
+# students.each_value do |value|
+#   puts value * 1.05
+# end
+#
+# students.delete(:cohort2)
+# puts students
+
+total_students = 0
+students.each do |key, value|
+  total_students += value
 end
 
-puts students.keys
-
-students.each_value do |value|
-  puts value * 1.05
-end
-
-students.delete(:cohort2)
-puts students
+puts total_students
