8797d16efec19e0cb10a0f386969789ffabf0971
CALLUM BURNS <CBU787@GMAIL.COM>
2016-03-23 15:38:29 -0400

    Added statement about bananas with if-else statement with .include.

--- exercise6.rb
+++ exercise6.rb
@@ -4,6 +4,12 @@ def list
   grocery_list.map! { |item| "* " + item }
   puts grocery_list
   puts grocery_list.length
+  if
+    grocery_list.include? "bananas"
+    puts "You need to pick up bananas."
+  else
+    puts "You don't need to pick up bananas today."
+  end
 end
 
 list
