6fe0882233fde78e7dfbd64903d35c8c997da32f
CALLUM BURNS <CBU787@GMAIL.COM>
2016-03-23 15:48:39 -0400

    Removed salmon from list using .delete_at.

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,7 @@
 def list
   grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
   grocery_list << "rice"
+  grocery_list.delete_at(3)
   grocery_list.sort!
   grocery_list.map! { |item| "* " + item }
   puts grocery_list
