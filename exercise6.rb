79529aea9bad91b60849fb1c6f51eb4ace828279
CALLUM BURNS <CBU787@GMAIL.COM>
2016-03-23 15:32:26 -0400

    Added method, counted number of items in list using .length.

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,9 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+def list
+  grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+  grocery_list << "rice"
+  grocery_list.map! { |item| "* " + item }
+  puts grocery_list
+  puts grocery_list.length
+end
 
-grocery_list << "rice"
-grocery_list.map! { |item| "* " + item }
-puts grocery_list
+list
