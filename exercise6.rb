7749636322e833fb51ebefceaa2c01ebd4db8d67
CALLUM BURNS <CBU787@GMAIL.COM>
2016-03-23 15:43:17 -0400

    Sorted items alphabetically using .sort.

--- exercise6.rb
+++ exercise6.rb
@@ -1,15 +1,18 @@
 def list
   grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
   grocery_list << "rice"
+  grocery_list.sort!
   grocery_list.map! { |item| "* " + item }
   puts grocery_list
   puts grocery_list.length
+
   if
     grocery_list.include? "bananas"
     puts "You need to pick up bananas."
   else
     puts "You don't need to pick up bananas today."
   end
+
 end
 
 list
